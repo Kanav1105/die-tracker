ff5e95ea92db5c164057842808959daedf4fb673
Kanav1105 <148464801+Kanav1105@users.noreply.github.com>
2026-07-28 08:47:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Die Tracker service worker.
    Bump CACHE on every deploy or tablets will keep the old build. */
-const CACHE = "die-tracker-v4";
+const CACHE = "die-tracker-v5";
 const SHELL = [
   "./", "./index.html", "./manager.html", "./core.js",
   "./manifest.json", "./manager-manifest.json",
